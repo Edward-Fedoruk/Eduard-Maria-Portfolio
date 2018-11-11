b3c6adb1bda3f403db84fbce66ce051793d05b5c
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-11-11 20:52:44 +0200

    configured partical js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 particlesJS.load('particles-js', '../libs/particles/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+particlesJS.load('particle', '../libs/particles/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+$( window ).on("load", function(e) {
+  
+})
