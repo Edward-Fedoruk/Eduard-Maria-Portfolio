34cfd239baf6d1d2030b4d82eadab5082d546146
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-12-15 09:07:01 +0200

    main navigation

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,23 @@ particlesJS.load('particle', '../libs/particles/particlesjs-config.json')
 $( window ).on("load", function(e) {
   AOS.init({
     duration: 1200,
+    once: true
   })
+})
+
+let startOffsetY = $(".navigation").offset().top
+$(window).on("scroll", function(e) {
+  if (
+    $(window).scrollTop() - $(".navigation").height() - startOffsetY < 
+    $(".navigation").offset().top - $(window).scrollTop()
+  ) 
+    $(".navigation").css({top: (startOffsetY - $(window).scrollTop()/2) + 'px' })
+})
+
+$(".navigation__navItem").on("click", function(e) {
+  let section = $(this).data("link")
+  console.log(section)
+  $('html, body').animate({
+      scrollTop: $(section).offset().top
+  }, 2000, 'swing')
 })
