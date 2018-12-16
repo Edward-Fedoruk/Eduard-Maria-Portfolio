803e51800261ca6eaaba097406a66ab601523d84
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-12-16 10:18:40 +0200

    minor fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,224 @@
-particlesJS.load('particles-js', '../libs/particles/particlesjs-config.json')
+particlesJS.load('particles-js', {
+  "particles": {
+    "number": {
+      "value": 380,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+})
 
-particlesJS.load('particle', '../libs/particles/particlesjs-config.json')
+particlesJS.load('particle', {
+  "particles": {
+    "number": {
+      "value": 380,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+})
 
 $( window ).on("load", function(e) {
   AOS.init({
@@ -18,14 +236,38 @@ $(window).on("scroll", function(e) {
     $(".navigation").css({top: (startOffsetY - $(window).scrollTop()/2) + 'px' })
 })
 
+
+let sectionOffsets = []
+$(".navigation__navItem").each(function(e) {
+  const sectionClass = $(this).data("link")
+  sectionOffsets.push($(sectionClass).offset().top)
+})
+
+let prevIndex = -1
+const win = $(window)
+let interval = null
+$(window).on("scroll", function(e) {
+  clearTimeout(interval)
+  interval = setTimeout(function() {
+
+    const windowOffsetY = win.scrollTop()
+    const sectionIndex = sectionOffsets.findIndex(pos => windowOffsetY <= pos)
+
+    if(prevIndex === sectionIndex) return
+
+    const prevMenuItem = $(".navigation__navItem .hr").get(prevIndex)
+    const currMenuItem = $(".navigation__navItem").get(sectionIndex)
+
+    $(prevMenuItem).removeClass("hr-active")
+    $(currMenuItem).find(".hr").addClass("hr-active")
+
+    prevIndex = sectionIndex
+
+  }, 10)
+})
+
 $(".navigation__navItem").on("click", function(e) {
   let section = $(this).data("link")
-  
-  $(".navigation__navItem .hr").each(function(e) {
-    $(this).removeClass("hr-active")
-  })
-
-  $(this).find(".hr").addClass("hr-active")
 
   $('html, body').animate({
       scrollTop: $(section).offset().top
